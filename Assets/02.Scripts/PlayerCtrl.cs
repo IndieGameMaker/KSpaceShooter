69236a3423182fb8165e070b76191ad5f8c7e231
Lee JaeHyun <myleje@gmail.com>
2020-09-18 17:01:10 +0900

    Project upload

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float h;
+    private float v;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");   // -1.0f ~ 0.0f ~ +1.0f
+        v = Input.GetAxis("Vertical");     // -1.0f ~ 0.0f ~ +1.0f 
+        Debug.Log("h=" + h);    //Console View 로그 출력 
+        Debug.Log("V=" + v);
+    }
+}
