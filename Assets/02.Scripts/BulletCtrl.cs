adf915cb34a8c489fbe880a11dea5bc2cb2a9ec4
Lee JaeHyun <myleje@gmail.com>
2020-09-24 13:46:46 +0900

    .

---
+++ Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * 1000.0f);        
+    }
+
+}
