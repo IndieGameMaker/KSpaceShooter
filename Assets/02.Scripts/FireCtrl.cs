cd60e6c7729559fb89dd333a4dfa65224d79ca62
Lee JaeHyun <myleje@gmail.com>
2020-09-24 15:58:11 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -6,10 +6,13 @@ public class FireCtrl : MonoBehaviour
 {
     public Transform firePos;
     public GameObject bulletPrefab;
+
+    public AudioSource _audio;
+    public AudioClip fireSfx;
     
     void Start()
     {
-        
+        _audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,7 +20,17 @@ public class FireCtrl : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+            Fire();
         }
+    }
+
+    void Fire()
+    {
+        //Bullet 생성
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        //총소리 발생
+        // _audio.clip = fireSfx;
+        // _audio.Play();
+        _audio.PlayOneShot(fireSfx, 0.8f);
     }
 }
