cac28181b60e61ac56be0ae1eb60ec62eaa406f8
Lee JaeHyun <myleje@gmail.com>
2020-09-24 14:40:19 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -15,6 +15,9 @@ public class FireCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }
---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == "BULLET")
+        {
+            Destroy(coll.gameObject); //Bullet
+            //Destroy(this.gameObject); //Wall Destroy
+        }
+    }
+}
