7cb1d9b27f8ef2c8c7fd13061572490338d232ec
Lee JaeHyun <myleje@gmail.com>
2020-09-24 14:22:57 +0900

    .

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    public Transform firePos;
+    public GameObject bulletPrefab;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
