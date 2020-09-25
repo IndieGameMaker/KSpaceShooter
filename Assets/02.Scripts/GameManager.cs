51836e0669a8a86d79eb8f450c4d30821fdfd3d4
Lee JaeHyun <myleje@gmail.com>
2020-09-25 17:30:14 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -13,7 +13,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject spawnPointGroup = GameObject.Find("SpawnPointGroup");
+        points = spawnPointGroup.GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame
