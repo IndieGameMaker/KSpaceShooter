14262fe24d401658e8bc8a7394fa7bb337bedd4c
Lee JaeHyun <myleje@gmail.com>
2020-09-25 17:27:49 +0900

    .

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform[] points;
+    public GameObject monsterPrefab;
+    public float createTime = 3.0f;
+
+    public bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
